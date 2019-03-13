cf85bca9b2167e8ddc8c1a7f9c40a176ed4081e5
mloef <maxsloeffler@gmail.com>
2019-03-13 21:33:22 +0000

    possible working implementation

--- src/semaphoreBarrier.java
+++ src/semaphoreBarrier.java
@@ -6,50 +6,53 @@ public class semaphoreBarrier implements Barrier
     private Semaphore atomInt;
     int n; 
     int index; 
+    int gen;
 
     public semaphoreBarrier(int N)
     {   
         sems = new Semaphore[N-1];
         for (int i = 0; i < N-1; ++i) {
-            sems[i] = new Semaphore(0);
+            sems[i] = new Semaphore(0, true);
         }
-        atomInt = new Semaphore(1);
+        atomInt = new Semaphore(1, true);
         index = 0;
         n = N; 
+        gen = 0;
     }   
 
     public void arriveAndWait()
     {
         try{atomInt.acquire();}
         catch(InterruptedException e){
-            System.out.println("wait interrupted!");
+            //System.out.println("wait interrupted!");
         }
         
+        //System.out.println("Thread " + Thread.currentThread().getId() + " entering");
+        //System.out.flush();
         int t = index;
-        System.out.println("Thread " + t + " entering");
-        System.out.flush();
         index += 1;
         
         if (t < n-1) { 
+            //System.out.println("Thread " + Thread.currentThread().getId() + " in wait");
+            //System.out.flush();
             atomInt.release();
-            System.out.println("Thread " + t + " in wait");
-            System.out.flush();
             try{sems[t].acquire();}
             catch(InterruptedException e){
-                System.out.println("wait interrupted!");
+                //System.out.println("wait interrupted!");
             }
-            System.out.println("Thread " + t + " released");
-            System.out.flush();
+            //System.out.println("Thread " + Thread.currentThread().getId() + " released");
+            //System.out.flush();
         } 
         
         else {
+            index = 0;
+            //System.out.println("Thread " + Thread.currentThread().getId() + " in releasing");
+            //System.out.flush();
             for (int i = 0; i < n-1; ++i) {
                 sems[i].release();
             }
-            index = 0; 
             atomInt.release();
-            System.out.println("Thread " + t + " in releasing");
-            System.out.flush();
+            gen++;
         }
     }
 }
