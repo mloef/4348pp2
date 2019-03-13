82bed083954aae64f177f1d5bde1a63b3429b3eb
John Chan <john7chan@gmail.com>
2019-03-12 20:15:10 -0500

    added comments to monitorBarrier

--- src/monitorBarrier.java
+++ src/monitorBarrier.java
@@ -5,18 +5,24 @@ public class monitorBarrier implements Barrier
     
     public monitorBarrier(int N)
     {
+        //assigns proper values to private variables
         numThreads = N;
         count = 0;
     }
     
     public synchronized void arriveAndWait()
     {
+        //increments the counter of arrived threads
         count++;
+
+        //when all threads have arrived
         if (count == numThreads) {
+            //reset counter and resume all threads
             count = 0;
             notifyAll();
         }   
         else{
+            //when not all threads have arrived, freeze current thread
             try{wait();}
             catch(InterruptedException e){
                 System.out.println("wait interrupted!");
