f2ec3ebf89bca0e3a7c5ee7def40c433441e4e05
mloef <msl@DESKTOP-N6R8E6O.localdomain>
2019-03-12 19:41:43 -0500

    changed semaphore to TA implementation. TODO: fix race

--- src/semaphoreBarrier.java
+++ src/semaphoreBarrier.java
@@ -2,31 +2,45 @@ import java.util.concurrent.*;
 
 public class semaphoreBarrier implements Barrier
 {
-    private Semaphore sem;
-    private int n;
-    private final Object lock = new Object();
+    private Semaphore[] sems;
+    private Semaphore atomInt;
+    int n; 
+    int index; 
 
     public semaphoreBarrier(int N)
     {   
-        sem = new Semaphore(N-1);
+        sems = new Semaphore[N-1];
+        for (int i = 0; i < N-1; ++i) {
+            sems[i] = new Semaphore(0);
+        }
+        atomInt = new Semaphore(1);
+        index = 0;
         n = N; 
     }   
-    
+
     public void arriveAndWait()
-    {   
-        synchronized(lock) { 
-            if (sem.tryAcquire()) {
-                try{
-                    lock.wait();
-                }   
-                catch(InterruptedException e){ 
-                    System.out.println("wait interrupted!");
-                }   
-            }   
-            else {
-                lock.notifyAll();
-                sem.release(n-1);
-            }   
-        }   
-    }   
+    {
+        try{atomInt.acquire();}
+        catch(InterruptedException e){
+            System.out.println("wait interrupted!");
+        }
+        
+        int t = index;
+        index += 1;
+        atomInt.release();
+        
+        if (t < n-1) { 
+            try{sems[t].acquire();}
+            catch(InterruptedException e){
+                System.out.println("wait interrupted!");
+            }
+        } 
+        
+        else {
+            for (int i = 0; i < n-1; ++i) {
+                sems[i].release();
+            }
+            index = 0; 
+        }
+    }
 }
